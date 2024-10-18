c039ad087208c5409e3d748896f4c886b68f948c
karimatwa <karim-atwa@hotmail.com>
2024-10-18 15:24:30 +0200

    add login

--- src/test/java/ecommerce/EcommSimulation.java
+++ src/test/java/ecommerce/EcommSimulation.java
@@ -12,13 +12,16 @@ public class EcommSimulation extends Simulation {
 
   static final String baseUrl = "https://api-ecomm.gatling.io";
 
+  static final FeederBuilder<Object> usersFeeder = jsonFile("users.json").circular();
+
   static final HttpProtocolBuilder httpProtocolBase =
       http.baseUrl(baseUrl)
           .acceptHeader("application/json")
           .userAgentHeader(
               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0");
 
-  static final ScenarioBuilder scn = scenario("scenario 1").exec(homePage, session, products);
+  static final ScenarioBuilder scn =
+      scenario("scenario 1").exec(homePage, session, products, feed(usersFeeder), login);
 
   {
     setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocolBase);
--- src/test/java/ecommerce/endpoints/APIendpoints.java
+++ src/test/java/ecommerce/endpoints/APIendpoints.java
@@ -1,16 +1,15 @@
 package ecommerce.endpoints;
 
-import static io.gatling.javaapi.core.CoreDsl.jmesPath;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
 import io.gatling.javaapi.http.HttpRequestActionBuilder;
 
 public class APIendpoints {
   public static final HttpRequestActionBuilder session =
-      http("Session").get("/session").check(status().in(200));
+      http("Session").get("/session").check(status().is(200));
 
   public static final HttpRequestActionBuilder products =
-      http("Products").get("/products").check(status().in(200));
+      http("Products").get("/products").check(status().is(200));
 
   public static final HttpRequestActionBuilder login =
       http("Login")
