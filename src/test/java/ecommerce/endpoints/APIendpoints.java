c7705f15fa2de0a524c40d1d88ec304d9ed6affd
karimatwa <karim-atwa@hotmail.com>
2024-10-18 15:13:53 +0200

    add login

--- src/test/java/ecommerce/endpoints/APIendpoints.java
+++ src/test/java/ecommerce/endpoints/APIendpoints.java
@@ -1,13 +1,22 @@
 package ecommerce.endpoints;
 
+import static io.gatling.javaapi.core.CoreDsl.jmesPath;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
 import io.gatling.javaapi.http.HttpRequestActionBuilder;
 
 public class APIendpoints {
   public static final HttpRequestActionBuilder session =
-      http("Session").get("/session").check(status().in(200, 304));
+      http("Session").get("/session").check(status().in(200));
 
   public static final HttpRequestActionBuilder products =
-      http("Products").get("/products").check(status().in(200, 304));
+      http("Products").get("/products").check(status().in(200));
+
+  public static final HttpRequestActionBuilder login =
+      http("Login")
+          .post("/login")
+          .asFormUrlEncoded()
+          .formParam("username", "#{username}")
+          .formParam("password", "#{password}")
+          .check(status().is(200));
 }
